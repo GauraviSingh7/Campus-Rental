b1d2bf6c83b42d684d0874e339717cd03a79490d
aishvibansal <aishvibansal@gmail.com>
2026-02-11 01:31:19 +0530

    Cleaned up main branch: frontend moved to separate branch

--- frontend/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // This tells the browser: "Don't show the default page, go to /login instead"
-  redirect('/login');
-}
