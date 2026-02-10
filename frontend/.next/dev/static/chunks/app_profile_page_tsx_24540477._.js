d8a524a26f8df4c6012f20545d1a46cdd5625870
aishvibansal <aishvibansal@gmail.com>
2026-02-11 01:26:20 +0530

    Cleaned up repository: ignored .next and .DS_Store

--- frontend/.next/dev/static/chunks/app_profile_page_tsx_24540477._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_dbd5cee4._.js"
-],
-    source: "dynamic"
-});
